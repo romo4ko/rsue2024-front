fbc3f173779249d95f38a6407c69e6e716815966
Александр Коваленко <alexanderkovalenko@MacBook-Air-Aleksandr-2.local>
2024-11-30 18:17:49 +0300

    joined level detail page

--- src/components/Levels/Level.vue
+++ src/components/Levels/Level.vue
@@ -1,23 +1,35 @@
 <script setup>
 import {Button, Textarea} from "primevue";
+import Stepper from 'primevue/stepper';
+import StepList from 'primevue/steplist';
+import StepPanels from 'primevue/steppanels';
+import Step from 'primevue/step';
+import StepPanel from 'primevue/steppanel';
 import {RouterLink, useRoute} from "vue-router";
 import useDetailCourseStore from "../../store/useDetailCourseStore.js";
-import useRole from "../../composables/useRole.js";
 import router from "../../router.js";
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
+import useDetailLevelStore from "../../store/useDetailLevelStore.js";
+import useRole from "../../composables/useRole.js";
 
 const courseStore = useDetailCourseStore();
+const levelStore = useDetailLevelStore();
+const role = useRole();
 const route = useRoute();
+
+// Массив для хранения значений каждого Textarea
+const answers = ref([]);
 
 const joinCourseHandler = () => {
   router.push({
     name: "Levels",
   });
-}
+};
 
-const onFormSubmit = () => {
-  return null;
-}
+const onFormSubmit = (index) => {
+  // Обработка отправки ответа для определенного задания
+  console.log(`Ответ для задания ${index + 1}:`, answers.value[index]);
+};
 
 onMounted(async () => {
   await courseStore.getCourse(route.params.id);
@@ -26,35 +38,58 @@ onMounted(async () => {
 
 <template>
   <div class="wrapper">
-    <RouterLink :to="'/courses/' + route.params.id + '/levels'"><- Вернуться к уровням</RouterLink>
+    <RouterLink :to="'/courses/' + route.params.id + '/levels'"><span class="pi pi-arrow-left"></span> Вернуться к
+      уровням
+    </RouterLink>
     <div class="title">
       <h2 class="text-5xl">
-        {{courseStore.course.name}}
+        {{ courseStore.course.name }}
       </h2>
       <h3 class="text-2xl">
-        Название урока
+        {{ levelStore.level.name }}
       </h3>
     </div>
     <div class="theoretic-part py-10">
-      Содежание теоретической части
-    </div>
-    <div class="py-10">
-      <h4 class="text-2xl">
-        Задание 1. Название задания:
-      </h4>
-      <div class="my-5">
-        Текст задания
-      </div>
-      <span>
-        Впишите решение ниже:
+      <span class="w-[784px]">
+      {{ levelStore.level.theory }}
       </span>
-      <form @submit="onFormSubmit" class="flex flex-col gap-4 w-1/2 my-5">
-        <div class="flex flex-col gap-1">
-          <Textarea name="address" rows="5" cols="30" style="resize: none" />
-        </div>
-        <Button type="submit" label="Submit" />
-      </form>
-
+    </div>
+    <div class="card flex justify-center">
+      <Stepper value="1" class="basis-[50rem]">
+        <StepList>
+          <Step v-for="(exercise, key) of levelStore.level.exercises" :value="String(key + 1)">
+            Задание {{ key + 1 }}
+          </Step>
+        </StepList>
+        <StepPanels>
+          <StepPanel v-for="(exercise, key) of levelStore.level.exercises" v-slot="{ activateCallback }"
+                     :value="String(key + 1)">
+            <div class="flex flex-col">
+              <div class="my-5">
+                {{ exercise.condition }}
+              </div>
+              <span v-if="role === 'student'">
+                Впишите решение ниже:
+              </span>
+              <div v-if="role === 'student'" class="flex flex-col gap-4 my-5">
+                <div class="flex flex-col gap-1">
+                  <Textarea
+                      v-model="answers[key]"
+                      rows="5"
+                      cols="30"
+                      style="resize: none"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="flex pt-2 gap-8 justify-center">
+              <Button v-if="role === 'student'" :disabled="!answers[key]" class="!px-4 w-full" label="Подтвердить"/>
+              <Button v-if="key + 1 !== levelStore.level.exercises.length" label="Далее" class="w-1/3" icon="pi pi-arrow-right" iconPos="right"
+                      @click="activateCallback(String(key + 2))"/>
+            </div>
+          </StepPanel>
+        </StepPanels>
+      </Stepper>
     </div>
   </div>
 </template>
@@ -66,5 +101,9 @@ onMounted(async () => {
 
 .title {
   @apply flex flex-col items-center mt-5 gap-5;
+}
+
+.theoretic-part {
+  @apply flex justify-center;
 }
 </style>
