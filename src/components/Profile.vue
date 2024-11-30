e5c7a8554a31091f948b0e08d7209c003c012967
alexandr <alexandrignatushin@gmail.com>
2024-11-30 12:56:40 +0300

    Профиль учителя

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ParentProfileView from "./ParentProfileView.vue";
 import ChildProfileView from "./ChildProfileView.vue";
+import TeacherProfileView from "./TeacherProfileView.vue";
 </script>
 
 <template>
   <!--  <parent-profile-view />-->
-  <child-profile-view />
+<!--  <child-profile-view />-->
+  <teacher-profile-view />
 </template>
 
 <style scoped>
